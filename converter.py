359f2fb813b24c579b69a9c9f459937e68c8226a
Joost Grunwald <joostgrunwald2001@gmail.com>
2022-02-01 09:40:17 +0100

    implemented file iteration for sample

--- converter.py
+++ converter.py
@@ -1,13 +1,29 @@
 #pip install docx2txt
 import docx2txt
+import os
 
-# extract text
-text = docx2txt.process("AD-april-0005.docx")
+#SETTINGS
+directory = "C:\Users\joost\Desktop"
 
-# correct for double newlines present
-text = text.replace("\n\n", "\n")
-text = text.replace("\n\n", "\n")
+#MAIN
+def main()
+    output_directory = directory + "\txt" 
 
-# print text
-with open('readme.txt', 'w') as f:
-    f.write(text)
+    for filename in os.listdir(directory):
+        if filename.endswith(".docx"):
+             # print(os.path.join(directory, filename))
+            continue
+        else:
+            continue
+    # extract text
+    text = docx2txt.process("AD-april-0005.docx")
+
+    # correct for double newlines present
+    text = text.replace("\n\n", "\n")
+    text = text.replace("\n\n", "\n")
+
+    # print text
+    with open('readme.txt', 'w') as f:
+        f.write(text)
+
+main()
