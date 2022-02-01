771698d11b9f687a6af12f98f27381c28a81e3c9
Joost Grunwald <joostgrunwald2001@gmail.com>
2022-02-01 09:48:58 +0100

    implemented folder making

--- converter.py
+++ converter.py
@@ -3,18 +3,23 @@ import docx2txt
 import os
 
 #SETTINGS
-directory = "C:\\Users\\joost\\Desktop"
+directory = "C:\\Users\\joost\\Desktop\\werkmap"
 
 #MAIN
 def main():
 
-    #we write the converted file to here
-    output_directory = directory + "\\txt" 
+    #we write the converted file to this path
+    output_directory = directory + "\\txt"
 
+    #we first check if the path exists and make it if it does not exist
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
+        
     for filename in os.listdir(directory):
+        print(filename)
         if filename.endswith(".docx"):
             #DEBUG
-            # print(os.path.join(directory, filename))
+            print(os.path.join(directory, filename))
              
             # extract text
             input_path = directory + "\\" + filename
@@ -22,11 +27,14 @@ def main():
 
             # correct for double newlines present
             text = text.replace("\n\n", "\n")
-            text = text.replace("\n\n", "\n")
+            #text = text.replace("\n\n", "\n")
+
+            print(text)
 
             # print text
             output_filename = filename.replace(".docx",".txt")
             output_path = output_directory + "\\" + output_filename
+            
             with open(output_path, 'w') as f:
                 f.write(text)
 
