c104ab13e33bb6cfe0a1e5cb301de0e2b4947a42
Joost Grunwald <joostgrunwald2001@gmail.com>
2022-02-01 09:44:05 +0100

    first batch working version

--- converter.py
+++ converter.py
@@ -7,23 +7,27 @@ directory = "C:\Users\joost\Desktop"
 
 #MAIN
 def main()
+
+    #we write the converted file to here
     output_directory = directory + "\txt" 
 
     for filename in os.listdir(directory):
         if filename.endswith(".docx"):
-             # print(os.path.join(directory, filename))
-            continue
-        else:
-            continue
-    # extract text
-    text = docx2txt.process("AD-april-0005.docx")
+            #DEBUG
+            # print(os.path.join(directory, filename))
+             
+            # extract text
+            input_path = directory + "\\" + filename
+            text = docx2txt.process(input_path)
 
-    # correct for double newlines present
-    text = text.replace("\n\n", "\n")
-    text = text.replace("\n\n", "\n")
+            # correct for double newlines present
+            text = text.replace("\n\n", "\n")
+            text = text.replace("\n\n", "\n")
 
-    # print text
-    with open('readme.txt', 'w') as f:
-        f.write(text)
+            # print text
+            output_filename = filename.replace(".docx",".txt")
+            output_path = output_directory + "\\" + output_filename
+            with open(output_path, 'w') as f:
+                f.write(text)
 
-main()
+        main()
